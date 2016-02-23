7cd305b0547dab1d00297f0f7c22d331f799a3ec
allen_support <allen.support@webfactories.biz>
2016-02-23 14:42:37 +0800

    add stats section, removed portfolio section, removed disqus

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,47 @@
 $(function() {
   cbpBGSlideshow.init();
 
-  var d = document, s = d.createElement('script');
-
-  s.src = '//templateportfolio.disqus.com/embed.js';
-
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
+  $('.stats-bar').appear();
+    $('.stats-bar').on('appear', function() {
+    var fx = function fx() {
+    $(".stat-number").each(function (i, el) {
+        var data = parseInt(this.dataset.n, 10);
+        var props = {
+            "from": {
+                "count": 0
+            },
+                "to": {
+                "count": data
+            }
+        };
+        $(props.from).animate(props.to, {
+            duration: 1000 * 1,
+            step: function (now, fx) {
+                $(el).text(Math.ceil(now));
+            },
+            complete:function() {
+                if (el.dataset.sym !== undefined) {
+                  el.textContent = el.textContent.concat(el.dataset.sym)
+                }
+            }
+        });
+    });
+    };
+    
+    var reset = function reset() {
+        console.log($(this).scrollTop())
+        if ($(this).scrollTop() > 90) {
+            $(this).off("scroll");
+          fx()
+        }
+    };
+    
+    $(window).on("scroll", reset);
+});
 
 });
+
+
 
 $(document).ready(function(){
 
